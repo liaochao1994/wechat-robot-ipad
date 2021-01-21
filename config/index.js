9cb6f1c73247fad8725c2223055a6fe07c4eb31d
廖超 <44768520+liaochao1994@users.noreply.github.com>
2021-01-21 14:51:25 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@
  */
 module.exports = {
   TOKEN: "",
-  IGNORE: ["www.iiter.cn"], //忽略某个用户的消息，填写用户昵称即可
+  IGNORE: ["nickname"], //忽略某个用户的消息，填写用户昵称即可
   WEBROOM: "技术交流群", //要管理的群名称
-  MYSELF: "Peanut" //大号的备注！！！备注不是昵称！防止其他人冒充你哦
+  MYSELF: "nickname" //大号的备注！！！备注不是昵称！防止其他人冒充你哦
 };
